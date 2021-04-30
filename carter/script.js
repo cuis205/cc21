73c82d0dc6698a9e46cb37381ac85ffe9057ed03
Stephanie Cui <stephaniecui@Stephanies-MacBook-Pro.local>
2021-04-30 19:08:34 -0400

    hi

--- carter/script.js
+++ carter/script.js
@@ -1,7 +1,7 @@
 function buildPage(DATA) {
   vids = DATA.sort(function(a, b) {
-    var colourA = a.Order.toUpperCase(); // ignore upper and lowercase
-    var colourB = b.Order.toUpperCase(); // ignore upper and lowercase
+    var colourA = a.Order.toUpperCase();
+    var colourB = b.Order.toUpperCase();
     if (colourA < colourB) {
       return -1;
     }
@@ -9,13 +9,12 @@ function buildPage(DATA) {
       return 1;
     }
 
-    // names must be equal
+
     return 0;
   });
 
   var row;
-  // for (var i=0; i<films.length; i++){
-  //function scope
+
   Array.from(vids).forEach((objects, i) => {
     if (i < 2) {
       row = document.querySelector(".rowone");
@@ -26,18 +25,20 @@ function buildPage(DATA) {
 
     console.log(vids);
 
-    //var el = document.createElement('div');
     var tiles = vids[i];
     var video = document.createElement("VIDEO");
-    video.setAttribute("src", `${tiles.Video}`); // The image source from JSON array.
-    //el.appendChild(video);
+    video.setAttribute("src", `${tiles.Video}`);
     video.setAttribute("class", "sizing");
-    //video.setAttribute("controls", "controls");
     video.setAttribute("autoplay", "autoplay");
     video.setAttribute("loop", "loop");
     row.appendChild(video);
 
-    //el.addEventListener("click", function() {
-    //popUp(objects);
+  video.addEventListener('click', function() {
+    console.log("hi");
+    rand = Math.floor(Math.random() * 6) + 1 ;
+    video.src = "videos/00" + rand + ".mov";
+    console.log(video.src);
+  });
+
   });
 }
